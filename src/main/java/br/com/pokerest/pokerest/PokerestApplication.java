59542c14eed30d15f1795e9f1a97d36cf934b707
gmar02 <gmarinho2002@hotmail.com>
2026-03-31 12:03:41 -0400

    chore: commit inicial

---
+++ src/main/java/br/com/pokerest/pokerest/PokerestApplication.java
@@ -0,0 +1,13 @@
+package br.com.pokerest.pokerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pokerest/pokerest/PokerestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pokerest.pokerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
